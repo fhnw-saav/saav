16c95b34d18fa5b4e070204bb7d8453e3d87242d
Rahel Lüthy <rahel.luethy@gmail.com>
2016-05-23 17:32:00 +0200

    Add PoC for CSV file upload -> D3 > PDF report download

--- project/Settings.scala
+++ project/Settings.scala
@@ -6,11 +6,15 @@ object Settings {
   object versions {
     val saav = "0.1-SNAPSHOT"
     val scala = "2.11.8"
-    val scalaDom = "0.9.0"
+    val dom = "0.9.0"
+    val tags = "0.5.4"
+    val d3 = "0.3.3"
   }
 
   val libraryDependencies = Def.setting(Seq(
-    "org.scala-js" %%% "scalajs-dom" % versions.scalaDom
+    "org.scala-js" %%% "scalajs-dom" % versions.dom,
+    "com.lihaoyi" %% "scalatags" % versions.tags,
+    "org.singlespaced" %%% "scalajs-d3" % versions.d3
   ))
 
 }
