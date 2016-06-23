8da0bb6cd04d672a2df55ef645d4f71437c61ffa
Rahel Lüthy <rahel.luethy@gmail.com>
2016-06-23 18:56:05 +0200

    #13: Fix PDF rendering on Firefox and Safari (using canvg lib)

--- src/main/scala/ch/fhnw/ima/saav/component/PdfExportComponent.scala
+++ src/main/scala/ch/fhnw/ima/saav/component/PdfExportComponent.scala
@@ -7,8 +7,8 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 import japgolly.scalajs.react.{Callback, ReactComponentB}
 import org.scalajs.dom
 import org.scalajs.dom._
-import org.scalajs.dom.html.{Canvas, Image}
-import org.scalajs.dom.raw.{Blob, XMLSerializer}
+import org.scalajs.dom.html.Canvas
+import org.scalajs.dom.raw.XMLSerializer
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSName
@@ -16,71 +16,51 @@ import scalacss.ScalaCssReact._
 
 object PdfExportComponent {
 
-  private val idPrefix = PdfExportComponent.getClass.getSimpleName
-  private val hiddenImageId = idPrefix + "-hidden-image"
-  private val hiddenCanvasId = idPrefix + "-hidden-canvas"
-
   @JSName("URL")
   @js.native
   object URL extends dom.URL
 
   // Exporting an SVG tree to a PNG is pretty cumbersome:
-  // 1. build an SVG/XML string representation
-  // 2. use an HTML img node to load the SVG/XML string representation
-  // 3. render the HTML img node onto an HTML canvas
-  // 4. export the HTML canvas to PNG
-  // 5. add PNG to PDF
+  // 1. build an SVG/XML string representation (including CSS)
+  // 2. render SVG string onto an HTML canvas (via canvg lib)
+  // 3. export the HTML canvas to PNG
+  // 4. add PNG to PDF
   private def generatePdf = Callback {
 
     val css = GlobalStyles
 
     val svgChart = document.querySelector("." + css.svgContentResponsive.htmlClass)
-    val width = svgChart.clientWidth
-    val height = svgChart.clientHeight
+    val width = svgChart.getBoundingClientRect().width
+    val height = svgChart.getBoundingClientRect().height
 
-    // 1. build an SVG/XML string representation
+    // 1. build an SVG/XML string representation (including CSS)
     val svg = svgChart.cloneNode(true)
     svg.insertBefore(createDefsWithInlinedCss(), svg.firstChild)
-
     val svgString = new XMLSerializer().serializeToString(svg)
-    val blob = new Blob(js.Array(svgString), BlobPropertyBag("image/svg+xml;charset=utf-8"))
-    val svgStringUrl = URL.createObjectURL(blob)
-    val img = document.getElementById(hiddenImageId).asInstanceOf[Image]
-    val canvas = document.getElementById(hiddenCanvasId).asInstanceOf[Canvas]
+    val canvas = document.createElement("canvas").asInstanceOf[Canvas]
 
     // take high-resolution displays into account (otherwise looks blurry on retina)
     val pixelRatio = window.devicePixelRatio.toInt
-    canvas.width = width * pixelRatio
-    canvas.height = height * pixelRatio
-    canvas.style.width = canvas.width + "px"
-    canvas.style.height = canvas.height + "px"
+    canvas.width = (width * pixelRatio).toInt
+    canvas.height = (height * pixelRatio).toInt
 
-    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    // 2. render SVG string onto an HTML canvas
+    canvg(canvas, svgString)
 
-    img.onload = { evt: Event =>
+    // 3. export the HTML canvas to PNG
+    val dataURL = canvas.toDataURL("image/png", 1.0)
 
-      // 3. render the HTML img node onto an HTML canvas
-      ctx.drawImage(img, 0, 0)
-      URL.revokeObjectURL(svgStringUrl)
+    val doc = new jsPDF("p", "px", "a4")
+    val x = 50
+    doc.text(x, 50, Pages.Page.ProjectAnalysisPage.displayName)
 
-      // 4. export the HTML canvas to PNG
-      val dataURL = canvas.toDataURL("image/png", 1.0)
+    // 4. add PNG to PDF
+    val scaling = 0.3
+    val imgWidth = (scaling * width).toInt
+    val imgHeight = (scaling * height).toInt
+    doc.addImage(dataURL, "png", x, 80, imgWidth, imgHeight)
 
-      // uncomment to download PNG
-      // window.location.href = dataURL.replace("image/png", "image/octet-stream")
-
-      val doc = new jsPDF("p", "px", "a4")
-      val x = 50
-      doc.text(x, 50, Pages.Page.ProjectAnalysisPage.displayName)
-
-      // 5. add PNG to PDF
-      doc.addImage(dataURL, "png", x, 80, img.naturalWidth, img.naturalHeight)
-
-      doc.save("Report.pdf")
-    }
-
-    // 2. use an HTML img node to load the SVG/XML string representation
-    img.src = svgStringUrl
+    doc.save("Report.pdf")
   }
 
   private def createDefsWithInlinedCss(): Element = {
@@ -88,7 +68,7 @@ object PdfExportComponent {
     val cssStyle = document.querySelector("style").cloneNode(true).asInstanceOf[Element]
     // include font (should actually inline complete bootstrap CSS, but this is just a PoC)
     cssStyle.innerHTML = "* { font-family: \"Helvetica Neue\",Helvetica,Arial,sans-serif; } " + cssStyle.innerHTML
-    // container for inlined CSS
+    // container for inlined CSS (according to SVG spec)
     val defsElement = document.createElement("defs")
     defsElement.appendChild(cssStyle)
     defsElement
@@ -100,17 +80,14 @@ object PdfExportComponent {
 
   private val component = ReactComponentB[Props](PdfExportComponent.getClass.getSimpleName)
     .render { _ =>
-      <.div(
-        <.button(css.defaultButton, ^.onClick --> generatePdf, "Generate PDF"),
-        <.canvas(css.hidden, ^.id := hiddenCanvasId),
-        <.img(css.hidden, ^.id := hiddenImageId)
-      )
+        <.button(css.defaultButton, ^.onClick --> generatePdf, "Generate PDF")
     }
     .build
 
   def apply(analysis: Analysis[Project]) = component(Props(analysis))
 
-  // Facade to jsPDF library --> needs more type-safety once we settle on a usage...
+  // Scala facades to 3rd party JS libs --> need more type-safety once we settle on a usage...
+
   @js.native
   @JSName("jsPDF")
   class jsPDF(orientation: String, unit: String, pageFormat: String) extends js.Object {
@@ -121,6 +98,12 @@ object PdfExportComponent {
 
     def save(fileName: String): js.Any = js.native
 
+  }
+
+  @js.native
+  @JSName("canvg")
+  object canvg extends js.Object {
+    def apply(canvas: Canvas, svg: String): Unit = js.native
   }
 
 }
