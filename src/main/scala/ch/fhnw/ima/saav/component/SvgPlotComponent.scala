c1d0724297f0ba9548e6fef4763761b7df21c31d
Rahel Lüthy <rahel.luethy@gmail.com>
2016-08-23 17:07:29 +0200

    #20: Wire basic user interactions (hovering of axis, pinning of entities)

--- src/main/scala/ch/fhnw/ima/saav/component/SvgPlotComponent.scala
+++ src/main/scala/ch/fhnw/ima/saav/component/SvgPlotComponent.scala
@@ -1,6 +1,8 @@
 package ch.fhnw.ima.saav.component
 
-import ch.fhnw.ima.saav.model.app.{DataModel, PlottableCategory, PlottableEntity}
+import ch.fhnw.ima.saav.controller.SaavController.UpdateEntityPinningAction
+import ch.fhnw.ima.saav.model.app.{DataModel, PlottableEntity}
+import ch.fhnw.ima.saav.model.domain.SubCategory
 import diode.react.ModelProxy
 import japgolly.scalajs.react.vdom.prefix_<^._
 import japgolly.scalajs.react.{BackendScope, ReactComponentB}
@@ -9,16 +11,20 @@ object SvgPlotComponent {
 
   case class Props(proxy: ModelProxy[DataModel])
 
-  type HoveredPoint = Option[(PlottableCategory, PlottableEntity)]
-
-  case class State(hoveredPoint: HoveredPoint = None)
+  case class State(hoveredSubCategory: Option[SubCategory] = None)
 
   class Backend($: BackendScope[Props, State]) {
 
-    def setHoveredPoint(hoveredPoint: HoveredPoint) =
-      $.modState(s => State(hoveredPoint))
+    def setHoveredSubCategory(hoveredSubCategory: Option[SubCategory]) =
+      $.modState(s => State(hoveredSubCategory))
 
-    def clearHoveredPoint() = $.modState(s => State(None))
+    def clearHoveredSubCategory() = $.modState(s => State(None))
+
+    def toggleEntityPinning(plottableEntity: PlottableEntity) =
+      $.props >>= { p =>
+        val pinnedOrNone = if (plottableEntity.isPinned) None else Some(plottableEntity.id)
+        p.proxy.dispatch(UpdateEntityPinningAction(pinnedOrNone))
+      }
 
     def render(p: Props, s: State) = {
 
@@ -35,7 +41,7 @@ object SvgPlotComponent {
 
       val layout = new QualityLayout(model, plotWidth, plotHeight)
 
-      val coordinateSystem = constructCoordinateSystem(model, plotHeight, layout)
+      val coordinateSystem = constructCoordinateSystem(model, s.hoveredSubCategory, plotHeight, layout)
       val entities = constructEntities(model, layout)
 
       // Assemble everything
@@ -49,7 +55,7 @@ object SvgPlotComponent {
     }
 
 
-    private def constructCoordinateSystem(model: DataModel, plotHeight: Int, layout: QualityLayout) = {
+    private def constructCoordinateSystem(model: DataModel, hoveredSubCategory: Option[SubCategory], plotHeight: Int, layout: QualityLayout) = {
 
       // create the criteria boxes
       val boxes = for (category <- model.categories) yield {
@@ -62,20 +68,25 @@ object SvgPlotComponent {
 
       // create the criteria axes
       val criteriaAxes = for (category <- model.categories) yield {
+        val containsHoveredSubCategory = category.subCategories.count(sc => hoveredSubCategory.contains(sc.id)) > 0
+        val stroke = if (containsHoveredSubCategory) "black" else "#cccccc"
         <.svg.line(
           ^.svg.x1 := layout.getCriteriaAxisX(category), ^.svg.y1 := layout.getCategoryAxisTopY,
           ^.svg.x2 := layout.getCriteriaAxisX(category), ^.svg.y2 := layout.getCategoryAxisBotY,
-          ^.svg.stroke := "#cccccc", ^.svg.strokeWidth := "1"
+          ^.svg.stroke := stroke, ^.svg.strokeWidth := "1"
         )
       }
 
       // create the subcriteria axes
       val subCriteriaAxes = for (category <- model.categories) yield {
         val axes = for (subCategory <- category.subCategories) yield {
+          val stroke = if (hoveredSubCategory.contains(subCategory.id)) "black" else "#cccccc"
           <.svg.line(
             ^.svg.x1 := layout.getSubCriteriaAxisX(subCategory), ^.svg.y1 := layout.getSubCategoryAxisTopY,
             ^.svg.x2 := layout.getSubCriteriaAxisX(subCategory), ^.svg.y2 := layout.getSubCategoryAxisBotY,
-            ^.svg.stroke := "#cccccc", ^.svg.strokeWidth := "1"
+            ^.svg.stroke := stroke, ^.svg.strokeWidth := "1",
+            ^.onMouseOver --> setHoveredSubCategory(Some(subCategory.id)),
+            ^.onMouseOut --> clearHoveredSubCategory()
           )
         }
         <.svg.g(axes)
@@ -83,130 +94,65 @@ object SvgPlotComponent {
 
       <.svg.g(boxes, criteriaAxes, subCriteriaAxes)
     }
-  }
 
+    private def constructEntities(model: DataModel, layout: QualityLayout) = {
+      val entities = for (plottableEntity <- model.rankedEntities) yield {
 
-  private def constructEntities(model: DataModel, layout: QualityLayout) = {
-    val entities = for (plottableEntity <- model.rankedEntities) yield {
+        var stroke =
+          if (plottableEntity.isPinned) "black"
+          else if (plottableEntity.isSelected) plottableEntity.color.hexValue
+          else "#cccccc"
 
-      var stroke = if (plottableEntity.isSelected) plottableEntity.color.hexValue else "#cccccc"
-      var strokeWidth = if (plottableEntity.isSelected) 2 else 1
+        var strokeWidth = if (plottableEntity.isSelected) 2 else 1
 
-      // create the criteria values lines
+        // create the criteria values lines
 
-      var coordString = "M"
-      var index = 0
-      for (category <- model.categories) {
-        val x = layout.getCriteriaAxisX(category)
-        val value = category.groupedValues(plottableEntity.id).get * (layout.getCategoryAxisBotY - layout.getCategoryAxisTopY) / 100.0
-        val y = layout.getCategoryAxisBotY - value
-
-        if (index == 1) coordString += " L"
-        coordString += " " + x + " " + y
-
-        index += 1
-      }
-      val criteriaValuesLine = <.svg.path(^.svg.d := coordString,
-        ^.svg.stroke := stroke, ^.svg.strokeWidth := strokeWidth, ^.svg.fill := "none"
-      )
-
-      // create the subcriteria values lines
-
-      coordString = "M"
-      index = 0
-      for (category <- model.categories) {
-        for (subCategory <- category.subCategories) {
-          val x = layout.getSubCriteriaAxisX(subCategory)
-          val value = subCategory.groupedValues(plottableEntity.id).get * (layout.getSubCategoryAxisBotY - layout.getSubCategoryAxisTopY) / 100.0
-          val y = layout.getSubCategoryAxisBotY - value
+        var coordString = "M"
+        var index = 0
+        for (category <- model.categories) {
+          val x = layout.getCriteriaAxisX(category)
+          val value = category.groupedValues(plottableEntity.id).get * (layout.getCategoryAxisBotY - layout.getCategoryAxisTopY) / 100.0
+          val y = layout.getCategoryAxisBotY - value
 
           if (index == 1) coordString += " L"
           coordString += " " + x + " " + y
 
           index += 1
         }
-      }
-      val subCriteriaValuesLine = <.svg.path(^.svg.d := coordString,
-        ^.svg.stroke := stroke, ^.svg.strokeWidth := strokeWidth, ^.svg.fill := "none"
-      )
-
-      <.svg.g(criteriaValuesLine, subCriteriaValuesLine)
-
-    }
-
-    entities
-  }
-
-
-  /*
-      def render(p: Props, s: State) = {
-
-        val m = p.proxy.value
-
-        // only relevant for aspect ratio and e.g. relative stroke width (svg will be scaled within parent element)
-        val plotWidth = 1000
-        val plotHeight = 200
-
-        val padding = 10
-
-        val background = <.svg.rect(
-          ^.svg.fill := "lightgrey",
-          ^.svg.x := "0", ^.svg.y := "0",
-          ^.svg.width := "100%", ^.svg.height := "100%")
-
-        val plot = {
-
-          val lineHeight = plotHeight - 2 * padding
-          val categoryLineDistance = (plotWidth - 2 * padding) / m.categories.length
-
-          for {
-            (category, categoryIndex) <- m.categories.zipWithIndex
-            x = (categoryIndex * categoryLineDistance) + (categoryLineDistance / 2.0) + padding
-          } yield {
-            val lineStartY = padding
-            val categoryLines = <.svg.line(
-              ^.svg.x1 := x, ^.svg.y1 := lineStartY,
-              ^.svg.x2 := x, ^.svg.y2 := lineStartY + lineHeight,
-              ^.svg.stroke := "black", ^.svg.strokeWidth := "1"
-            )
-
-            val entities = m.rankedEntities.filter(_.isSelected)
-            val entityPointDistance = lineHeight / entities.length
-
-            val entityPoints = for {
-              (entity, entityIndex) <- entities.zipWithIndex
-              y = (entityIndex * entityPointDistance) + lineStartY + (entityPointDistance / 2)
-            } yield {
-              val isHovered = s.hoveredPoint.contains((category, entity))
-              val pinnedOrHovered = if (entity.isPinned || isHovered) "black" else "transparent"
-              <.svg.circle(
-                ^.svg.cx := x, ^.svg.cy := y, ^.svg.r := 5,
-                ^.svg.fill := entity.color.hexValue,
-                ^.svg.strokeWidth := 2,
-                ^.svg.stroke := pinnedOrHovered,
-                ^.onMouseOver --> setHoveredPoint(Some((category, entity))),
-                ^.onMouseOut --> clearHoveredPoint()
-              )
-            }
-
-            <.svg.g(
-              categoryLines,
-              entityPoints
-            )
-          }
-
-        }
-
-        <.svg.svg(^.svg.viewBox := s"0 0 $plotWidth $plotHeight",
-          background,
-          plot
+        val criteriaValuesLine = <.svg.path(^.svg.d := coordString,
+          ^.svg.stroke := stroke, ^.svg.strokeWidth := strokeWidth, ^.svg.fill := "none",
+          ^.onClick --> toggleEntityPinning(plottableEntity)
         )
 
+        // create the subcriteria values lines
+
+        coordString = "M"
+        index = 0
+        for (category <- model.categories) {
+          for (subCategory <- category.subCategories) {
+            val x = layout.getSubCriteriaAxisX(subCategory)
+            val value = subCategory.groupedValues(plottableEntity.id).get * (layout.getSubCategoryAxisBotY - layout.getSubCategoryAxisTopY) / 100.0
+            val y = layout.getSubCategoryAxisBotY - value
+
+            if (index == 1) coordString += " L"
+            coordString += " " + x + " " + y
+
+            index += 1
+          }
+        }
+        val subCriteriaValuesLine = <.svg.path(^.svg.d := coordString,
+          ^.svg.stroke := stroke, ^.svg.strokeWidth := strokeWidth, ^.svg.fill := "none",
+          ^.onClick --> toggleEntityPinning(plottableEntity)
+        )
+
+        <.svg.g(criteriaValuesLine, subCriteriaValuesLine)
+
       }
-  */
 
+      entities
+    }
 
-
+  }
 
   private val component = ReactComponentB[Props](SvgPlotComponent.getClass.getSimpleName)
     .initialState(State())
