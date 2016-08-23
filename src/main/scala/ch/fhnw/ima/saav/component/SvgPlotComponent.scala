ea2434c176129eaf42601fc414fd4a55b8e0852d
Dominique <dominique.brodbeck@fhnw.ch>
2016-08-23 08:52:13 +0200

    Extract the layout class into its own file

--- src/main/scala/ch/fhnw/ima/saav/component/SvgPlotComponent.scala
+++ src/main/scala/ch/fhnw/ima/saav/component/SvgPlotComponent.scala
@@ -1,6 +1,6 @@
 package ch.fhnw.ima.saav.component
 
-import ch.fhnw.ima.saav.model.app.{PlottableCategory, PlottableEntity, PlottableQualityDataModel, PlottableSubCategory}
+import ch.fhnw.ima.saav.model.app.{PlottableCategory, PlottableEntity, PlottableQualityDataModel}
 import diode.react.ModelProxy
 import japgolly.scalajs.react.vdom.prefix_<^._
 import japgolly.scalajs.react.{BackendScope, ReactComponentB}
@@ -33,7 +33,7 @@ object SvgPlotComponent {
         ^.svg.x := "0", ^.svg.y := "0",
         ^.svg.width := "100%", ^.svg.height := "100%")
 
-      val layout = new Layout(model, plotWidth, plotHeight)
+      val layout = new QualityLayout(model, plotWidth, plotHeight)
 
       val coordinateSystem = constructCoordinateSystem(model, plotHeight, layout)
       val entities = constructEntities(model, layout)
@@ -48,7 +48,8 @@ object SvgPlotComponent {
 
     }
 
-    private def constructCoordinateSystem(model: PlottableQualityDataModel, plotHeight: Int, layout: Layout) = {
+
+    private def constructCoordinateSystem(model: PlottableQualityDataModel, plotHeight: Int, layout: QualityLayout) = {
 
       // create the criteria boxes
       val boxes = for (category <- model.categories) yield {
@@ -84,7 +85,8 @@ object SvgPlotComponent {
     }
   }
 
-  private def constructEntities(model: PlottableQualityDataModel, layout: Layout) = {
+
+  private def constructEntities(model: PlottableQualityDataModel, layout: QualityLayout) = {
     val entities = for (plottableEntity <- model.rankedEntities) yield {
 
       var stroke = if (plottableEntity.isSelected) plottableEntity.color.hexValue else "#cccccc"
@@ -204,91 +206,6 @@ object SvgPlotComponent {
   */
 
 
-  /**
-    * This class computes all the relevant layout parameters.
-    */
-
-  class Layout () {
-    val MARGIN = 10
-    val PADDING = 20
-    val VERTICAL_AXIS_GAP = 40
-
-    private var criteriaAxisTopY = 0
-    private var criteriaAxisBotY = 0
-    private var subCriteriaAxisTopY = 0
-    private var subCriteriaAxisBotY = 0
-
-    private val criteriaBoxesMap = new scala.collection.mutable.HashMap[PlottableCategory, (Int, Int)]
-    private val criteriaAxesMap = new scala.collection.mutable.HashMap[PlottableCategory, Int]
-    private val subCriteriaAxesMap = new scala.collection.mutable.HashMap[PlottableSubCategory, Int]
-
-    def this(model: PlottableQualityDataModel, width: Int, height: Int) {
-      this()
-
-      // Compute general parameters
-
-      val criteriaCount = computeCriteriaCount(model)
-      val axisCount = computeAxisCount(model)
-
-      val axisSpacing = (width - ((criteriaCount+1) * MARGIN) - (criteriaCount * 2 * PADDING)) / (axisCount - criteriaCount)
-
-      val axisHeight = (height - 2*PADDING - VERTICAL_AXIS_GAP) / 2
-
-      // Compute axes y positions
-
-      criteriaAxisTopY = PADDING
-      criteriaAxisBotY = PADDING + axisHeight
-
-      subCriteriaAxisTopY = height - PADDING - axisHeight
-      subCriteriaAxisBotY = height - PADDING
-
-      // Compute boxes and axes x positions
-
-      var index = 0
-      var x = 0
-      for (category <- model.categories) {
-
-        x = x + MARGIN
-        val criteriaWidth = 2*PADDING + ((category.subCategories.size-1) * axisSpacing)
-
-        criteriaBoxesMap(category) = (x, criteriaWidth)
-        criteriaAxesMap(category) = x + (criteriaWidth / 2)
-
-        var subIndex = 0
-        // intellij does not catch it if we forget the .subCategories
-        for (subCategory <- category.subCategories) {
-          subCriteriaAxesMap(subCategory) = x + PADDING + (subIndex * axisSpacing)
-          subIndex += 1
-        }
-
-        index += 1
-        x = x + criteriaWidth
-      }
-    }
-
-    def getCategoryAxisTopY = criteriaAxisTopY
-    def getCategoryAxisBotY = criteriaAxisBotY
-    def getSubCategoryAxisTopY = subCriteriaAxisTopY
-    def getSubCategoryAxisBotY = subCriteriaAxisBotY
-
-    def getCriteriaBox(category: PlottableCategory): (Int, Int) = criteriaBoxesMap(category)
-    def getCriteriaAxisX(category: PlottableCategory): Int = criteriaAxesMap(category)
-    def getSubCriteriaAxisX(subCategory: PlottableSubCategory): Int = subCriteriaAxesMap(subCategory)
-
-
-    private def computeCriteriaCount(model: PlottableQualityDataModel) = {
-      model.categories.size
-    }
-
-    private def computeAxisCount(model: PlottableQualityDataModel) = {
-      var count = 0
-      for (category <- model.categories) {
-        count += category.subCategories.size
-      }
-      count
-    }
-
-  }
 
 
   private val component = ReactComponentB[Props](SvgPlotComponent.getClass.getSimpleName)
