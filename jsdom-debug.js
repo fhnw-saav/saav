729ff478d8239983aae607b421a9335c627054df
Rahel Lüthy <rahel.luethy@gmail.com>
2016-11-14 17:19:28 +0100

    Revert "#70: Add jsdom-debug.js"
    This reverts commit 86144e8ac27b7aede010136cd81d30df6cc57fd4.

--- jsdom-debug.js
+++
@@ -1,9 +0,0 @@
-var jsdom = require("jsdom");
-
-jsdom.env(
-  "https://iojs.org/dist/",
-  ["http://code.jquery.com/jquery.js"],
-  function (err, window) {
-    console.log("there have been", window.$("a").length - 4, "io.js releases!");
-  }
-);
