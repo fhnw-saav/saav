492a532ea635ac1327f5f7a53ab7f581eb0a83fe
Rahel Lüthy <rahel.luethy@gmail.com>
2016-11-14 15:48:32 +0100

    #43: Gradually adding legend to PDF (work in progress)

--- src/main/scala/ch/fhnw/ima/saav/component/PdfExportComponent.scala
+++ src/main/scala/ch/fhnw/ima/saav/component/PdfExportComponent.scala
@@ -2,21 +2,29 @@ package ch.fhnw.ima.saav
 package component
 
 import ch.fhnw.ima.saav.component.bootstrap.{Button, Modal}
+import ch.fhnw.ima.saav.component.pages.PageWithDataComponent.{ProfileTab, QualityTab, Tab}
+import ch.fhnw.ima.saav.jspdf.jsPDF
+import ch.fhnw.ima.saav.model.app.AppModel
 import japgolly.scalajs.react.vdom.ReactTagOf
 import japgolly.scalajs.react.vdom.prefix_<^._
 import japgolly.scalajs.react.{Callback, ReactComponentB, _}
 import org.scalajs.dom
 import org.scalajs.dom._
+import org.scalajs.dom.ext.Color
 import org.scalajs.dom.html.{Canvas, Div}
 import org.scalajs.dom.raw.{HTMLImageElement, SVGSVGElement, XMLSerializer}
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.JSName
+import scala.concurrent.{Future, Promise}
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.util.Success
 import scalacss.ScalaCssReact._
 
 object PdfExportComponent {
 
-  case class Props(chartSvgRootElementId: String, defaultTitle: String)
+  private val FontSize = 10
+  private val LineHeight = 10
+
+  case class Props(chartSvgRootElementId: String, defaultTitle: String, activeTab: Tab, model: AppModel)
 
   case class State(showReportForm: Boolean = false, title: String)
 
@@ -31,7 +39,39 @@ object PdfExportComponent {
     // 2. render SVG string onto an HTML canvas
     // 3. export the HTML canvas to PNG
     // 4. add PNG to PDF
-    private def generatePdf(title: String) = Callback {
+    private def generatePdf(title: String, activeTab: Tab, model: AppModel) = Callback {
+
+      // A4 landscape
+      val landscape = "l"
+      val pdf = new jsPDF(landscape, "mm", "a4")
+
+      val chartImageFuture = exportChart()
+
+      chartImageFuture.foreach { (imageInfo: ImageInfo) =>
+        val mmPageMarginX = 10
+        val mmPageMarginY = 20
+
+        // Title
+        pdf.text(title, mmPageMarginX, mmPageMarginY)
+
+        val mmPageWidth = 297
+
+        val mmImageY = 30
+        val mmImageWidth = mmPageWidth - (2 * mmPageMarginX)
+        val mmImageHeight = imageInfo.aspectRatio * mmImageWidth
+        pdf.addImage(imageInfo.dataURL, "png", mmPageMarginX, mmImageY, mmImageWidth, mmImageHeight)
+
+        pdf.addPage()
+
+        appendLegend(pdf, mmPageMarginX, mmPageMarginY, activeTab, model)
+
+        pdf.save("Report.pdf")
+      }
+    }
+
+    case class ImageInfo(dataURL: String, aspectRatio: Int)
+
+    private def exportChart(): Future[ImageInfo] = {
       val svgChart = document.getElementById(ChartComponent.ElementId).asInstanceOf[SVGSVGElement]
       val width = svgChart.width.baseVal.value
       val height = svgChart.height.baseVal.value
@@ -51,30 +91,49 @@ object PdfExportComponent {
       val image = document.createElement("img").asInstanceOf[HTMLImageElement]
       image.src = "data:image/svg+xml," + svgString
       val ctx = canvas.getContext("2d")
+
+      val resultPromise = Promise[ImageInfo]()
+
       image.onload = (_: dom.Event) => {
         ctx.drawImage(image, 0, 0)
-
         // 3. export the HTML canvas to PNG
         val dataURL = canvas.toDataURL("image/png", 1.0)
-
-        // 4. add PNG to PDF
-        val landscape = "l"
-        val doc = new jsPDF(landscape, "mm", "a4")
-
-        // title
-        val mmPageMarginX = 10
-        val mmPageMarginY = 20
-        doc.text(mmPageMarginX, mmPageMarginY, title)
-
-        // chart image
-        val mmPageWidth = 297 // A4 landscape
-        val mmImageY = 30
-        val mmImageWidth = mmPageWidth - (2 * mmPageMarginX)
-        val mmImageHeight = (canvas.height / canvas.width) * mmImageWidth
-        doc.addImage(dataURL, "png", mmPageMarginX, mmImageY, mmImageWidth, mmImageHeight)
-
-        doc.save("Report.pdf")
+        val aspectRatio = canvas.height / canvas.width
+        resultPromise.complete(Success(ImageInfo(dataURL, aspectRatio)))
       }
+
+      resultPromise.future
+
+    }
+
+    private def appendLegend(pdf: jsPDF, mmPageMarginX: Int, mmPageMarginY: Int, activeTab: Tab, model: AppModel) = {
+
+      pdf.setFontSize(FontSize)
+
+      activeTab match {
+        case QualityTab =>
+          var x = mmPageMarginX
+          var y = mmPageMarginY
+          for (e <- model.qualityModel.rankedEntities) {
+            pdf.text(e.displayName, x, y)
+            val webColor = model.colorMap(e.id)
+
+            val color = Color(webColor.hexValue)
+            pdf.setFillColor(color.r, color.g, color.b)
+
+            pdf.rect(x + 100, y - 3, 6, 6, "F")
+            y += LineHeight
+          }
+
+        case ProfileTab =>
+          var x = mmPageMarginX
+          var y = mmPageMarginY
+          for (e <- model.profileModel.sortedEntities) {
+            pdf.text(e.displayName, x, y)
+            y += LineHeight
+          }
+      }
+
     }
 
     private def createDefsWithInlinedCss(): Element = {
@@ -105,7 +164,7 @@ object PdfExportComponent {
         val modal = Modal(
           Modal.Props(
             header = _ => <.h2(exportPdfLabel),
-            footer = hide => Button(generatePdf(s.title) >> hide >> hideReportForm, "OK")),
+            footer = hide => Button(generatePdf(s.title, p.activeTab, p.model) >> hide >> hideReportForm, "OK")),
           labelledTitleInput
         )
         <.div(button, modal)
@@ -123,20 +182,6 @@ object PdfExportComponent {
     .renderBackend[Backend]
     .build
 
-  def apply(chartSvgRootElementId: String, defaultTitle: String): ReactComponentU[Props, State, Backend, TopNode] = component(Props(chartSvgRootElementId, defaultTitle))
-
-  // Scala facades to 3rd party JS libs
-
-  @js.native
-  @JSName("jsPDF")
-  class jsPDF(orientation: String, unit: String, pageFormat: String) extends js.Object {
-
-    def addImage(url: String, imageType: String, x: Int, y: Int, width: Int, height: Int): js.Any = js.native
-
-    def text(x: Int, y: Int, text: String): js.Any = js.native
-
-    def save(fileName: String): js.Any = js.native
-
-  }
+  def apply(chartSvgRootElementId: String, defaultTitle: String, activeTab: Tab, model: AppModel): ReactComponentU[Props, State, Backend, TopNode] = component(Props(chartSvgRootElementId, defaultTitle, activeTab, model))
 
 }
